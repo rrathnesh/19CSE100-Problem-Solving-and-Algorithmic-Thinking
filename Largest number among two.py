6f504887b9e16030766d2d29c6c418312a816c9c
rrathnesh <116828306+rrathnesh@users.noreply.github.com>
2023-03-30 22:04:04 +0530

    Delete Largest number among two.py

--- Largest number among two.py
+++
@@ -1,8 +0,0 @@
-print("Enter the value of A")
-a = int(input())
-print("Enter the Values of B")
-b = int(input())
-if a > b:
-    print("A is the biggest number")
-else:
-    print("B is the biggest number")
